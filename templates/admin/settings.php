cad229d7f63d0c36e4e68406c34d1a1c406d8263
Ben Greene <benrgreene@gmail.com>
2018-07-24 12:07:27 -0700

    refactor: update theme setting template
    Added a table element into the theme settings page for better UI

--- templates/admin/settings.php
+++ templates/admin/settings.php
@@ -7,16 +7,18 @@
     
     <h2>Archives</h2>
     <?php $post_types = apply_filters( 'brg/archived_post_types', array() ); ?>
-    <?php foreach( $post_types as $post_type ): ?>
-      <?php $selected = get_option('brg_settings_display_' . $post_type); ?>
-      <p>
-        Display `<?php echo $post_type; ?>` post type as: 
-        <select name="brg_settings_display_<?php echo $post_type; ?>">
-          <option value="grid">Grid</option>
-          <option value="stacked"<?php if('stacked' == $selected): echo ' selected="selected"'; endif; ?>>Stacked</option>
-        </select>
-      </p>
-    <?php endforeach; ?>
+    <table>
+      <?php foreach( $post_types as $post_type ): ?>
+        <?php $selected = get_option('brg_settings_display_' . $post_type); ?>
+        <tr>
+          <td>Display `<?php echo $post_type; ?>` post type as:</td>
+          <td><select name="brg_settings_display_<?php echo $post_type; ?>">
+            <option value="grid">Grid</option>
+            <option value="stacked"<?php if('stacked' == $selected): echo ' selected="selected"'; endif; ?>>Stacked</option>
+          </select></td>
+        </tr>
+      <?php endforeach; ?>
+    </table>
 
     <?php submit_button(); ?>
   </form>
