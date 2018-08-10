bfba325847139c41ee88285dcaaf283952eb4aec
Ben Greene <benrgreene@gmail.com>
2018-08-10 15:03:58 -0700

    feat: add sidebars to the site
    Added the sidebars to the site.

--- core/single_page_loader.php
+++ core/single_page_loader.php
@@ -2,19 +2,23 @@
 
 class Single_Page_Loader {
 
-	function __construct() {
+	private $has_sidebar;
+
+	function __construct( $has_sidebar=true ) {
+		$this->has_sidebar = $has_sidebar;
 		the_post();
 	}
 	
 	public function the_page_layout() { ?>
-		<div class='l-contain single-page'>
-			<?php $this->main_content(); ?>
+		<div class="l-contain">
+		<div class="single-page <?php if( $this->has_sidebar ): echo 'single-page--sidebar'; endif; ?>">
+			<?php get_template_part( 'templates/fulls/full', get_post_format() ); ?>
+			<?php if( $this->has_sidebar ): ?>
+				<?php get_template_part( 'templates/partials/sidebar' ); ?>
+			<?php endif; ?>
+		</div>
 		</div>
 	<?php }
-
-	public function main_content() {
-		get_template_part( 'templates/fulls/full', get_post_format() );
-	}
 
 	public function the_page_hero() {
 		$hero_img_url = get_media_background( get_the_ID() );
--- core/widgets.php
+++ core/widgets.php
@@ -12,6 +12,15 @@ function brg_add_widgets() {
     'before_title'  => '<h3 class="footer-widgets__title">',
     'after_title'   => '</h3>',
   ) );
+
+  register_sidebar( array(
+    'name'          => 'Sidebar Widgets',
+    'id'            => 'brg_sidebar',
+    'before_widget' => '<div class="sidebar-widget">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3 class="sidebar-widget__title">',
+    'after_title'   => '</h3>',
+  ) );
 }
 
 add_action( 'after_setup_theme', 'brg_setup_menus' );
