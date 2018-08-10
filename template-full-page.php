ce8531f92115796b881e8d03c63ff7460bf2af8a
Ben Greene <benrgreene@gmail.com>
2018-08-10 15:06:49 -0700

    chore: add full page template

---
+++ template-full-page.php
@@ -0,0 +1,16 @@
+<?php
+
+// Template Name: Full Page (No Sidebar)
+
+include 'header.php';
+
+do_action( 'before_site_content' );
+
+// get hero image if available
+$page_loader = new Single_Page_Loader( false );
+$page_loader->the_page_hero();
+$page_loader->the_page_layout();
+
+do_action( 'after_site_content' );
+
+include 'footer.php';
