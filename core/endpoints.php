4c079a7240af9c607de937987e14028fe4de03be
Ben Greene <benrgreene@gmail.com>
2018-09-14 13:45:07 -0700

    bug: fix rest endpoint for archives
    Fixed issue with REST endpoint not existing for the archive's load more
    function, causing posts not being loaded

--- core/endpoints.php
+++ core/endpoints.php
@@ -28,3 +28,24 @@ function brg_get_search_results( WP_REST_Request $request ) {
 
   return $to_return;
 }
+
+// Add an endpoint for loading the posts
+add_action( 'rest_api_init', 'brg_get_next_posts_callback' );
+function brg_get_next_posts_callback() {
+  register_rest_route( 'post', 'get-posts/(?P<on>[0-9-]+)', array(
+    'methods' => 'GET',
+    'callback'  => 'brg_load_next_posts',
+  ) );
+}
+
+// Callback for REST endpoint to load the next page of results
+function brg_load_next_posts( $data ) {
+  $loader = new BG_Post_Archive_Delegator(
+    'post', 
+    $data['on']
+  );
+  
+  ob_start();
+  $loader->get_next_posts();
+  return ob_get_clean();
+}
