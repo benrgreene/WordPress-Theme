b40ad7108decacab2fa2fb394628d54d4edff9c4
Ben Greene <benrgreene@gmail.com>
2018-09-15 13:27:11 -0700

    feat: update mobile menu styles
    Updated the mobile menu styling to fix positioning and color

--- js/scripts.js
+++ js/scripts.js
@@ -66,6 +66,16 @@ const parentMenus = document.querySelectorAll('.sub-menu');
 const menuToggle  = document.querySelector('#menu-toggle');
 const menu        = document.querySelector('.menu-primary-menu-container');
 
+// set the menu's top to be equal to the height of the header for mobile styling
+document.addEventListener("DOMContentLoaded", (event) =>{
+  setTimeout(() => {
+    let styles = document.createElement('style');
+    let headerHeight = document.querySelector('.site-header').clientHeight;
+    styles.innerHTML = `@media(max-width: 770px){ .site-header .menu { top: ${headerHeight}px; }}`;
+    document.body.appendChild(styles);
+}, 10);
+});
+
 // Variable for tracking whether the menu is displayed or hidden
 let isDisplayed = false;
 
@@ -95,7 +105,6 @@ allButtons.forEach(function(el, idx) {
 
 // Toggle opening/closing the menu
 menuToggle.addEventListener('click', function(event) {
-	console.log('here');
   isDisplayed = !isDisplayed;
   var displayClass = isDisplayed ? 'block' : 'none';
   if(isDisplayed) {
